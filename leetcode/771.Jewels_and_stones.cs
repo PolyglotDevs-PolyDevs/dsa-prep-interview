0aa6b53573ae1fae5aa216c23b64a364eaad8103
hipster <santoscampos269@gmail.com>
2021-11-01 17:42:04 +0000

    jeweles and stoned c#

---
+++ leetcode/771.Jewels_and_stones.cs
@@ -0,0 +1,21 @@
+using  System; 
+using System.Collection.Generic; 
+
+namespace jewelsAndStones { 
+    public class Solution{
+        public Solution(){
+
+        }
+        public int jewels (string[] j){
+            string[] S = new [ 
+                'aAAbbbbb'
+            ]; 
+            int counter = 0; 
+            foreach(string c  in  s ){
+             counter = (c == j[1] || j[0])? counter++:counter=counter;
+            }
+            return counte
+
+        }
+    }
+}
